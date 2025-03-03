6d2e6cd9c426c75683e6c309fc891d2ff4dfb3af
群馬大学数理データ科学教育研究センター <gunmauniv.cmd@gmail.com>
2025-03-04 03:03:57 +0900

    Add files via upload

--- R7_install_add.sh
+++ R7_install_add.sh
@@ -12,19 +12,20 @@ echo "Nginx の設定を行います"
 cat <<EOL > /tmp/jupyterlab_nginx
 server {
     listen 80;
-    server_name $SERVER_HN.local $SERVER_IP;
-
+    server_name \$SERVER_HN.local \$SERVER_IP;
+    client_max_body_size 1000M;
     location / {
         proxy_pass http://127.0.0.1:8888/;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
-        proxy_redirect off;
-        proxy_buffering off;
+        # WebSocket サポート
+        proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection "Upgrade";
-        proxy_read_timeout 3600s;
+        proxy_set_header Connection "upgrade";
+
+        proxy_buffering off;
     }
 }
 EOL
