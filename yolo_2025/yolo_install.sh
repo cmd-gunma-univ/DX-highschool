2e2353fa6de7b8b591aa586f1fd8fbc127cd8205
群馬大学数理データ科学教育研究センター <gunmauniv.cmd@gmail.com>
2025-10-11 21:27:47 +0900

    Add files via upload

--- yolo_2025/yolo_install.sh
+++ yolo_2025/yolo_install.sh
@@ -1,6 +1,6 @@
-sudo apt update
+sudo apt update -y
 sudo apt remove -y python3-opencv
-sudo apt install python3-pip libopencv-dev
+sudo apt install -y python3-pip libopencv-dev --fix-missing
 pip3 install numpy ultralytics supervision deep-sort-realtime mediapipe moviepy==1.0.3 --break-system-packages
 pip3 install opencv-python opencv-python-headless opencv-contrib-python -y --break-system-packages
 pip install moviepy==1.0.3 --no-cache-dir --break-system-packages
